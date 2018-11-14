982e552b061cf311db8d97eeeffa3bc75386198e
sebita <ssamv.1998@gmail.com>
2018-11-14 12:55:44 -0300

    soy el seba cualquier cosa

--- ALfabeta/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from blog import views
-from django.conf import settings
-
-urlpatterns =[
-    path('',views.index,name='index'),
-    path('registrou',views.registrou,name='index')
-]
--- ALfabeta/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-  return render(request,'blog/index.html',{})
-
-def registrou(request):
-  return render(request,'blog/registrou.html',{})
-
